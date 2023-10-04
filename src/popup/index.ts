16ce67a5e80daceb6e4d7373a09a9e9cf92666ac
eladr7 <eladr7@gmail.com>
2023-10-04 15:56:36 +0300

    Made it work

---
+++ src/popup/index.ts
@@ -0,0 +1,38 @@
+const timeElement = document.getElementById("time");
+const timer = document.getElementById("timer");
+const userName = document.getElementById("name");
+userName!.textContent = "Your name is: blabla";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const timeElement = document.getElementById("time");
+  const timer = document.getElementById("timer");
+  const userName = document.getElementById("name");
+  userName!.textContent = "Your name is: blabla";
+});
+
+// const updateTimeElements = () => {
+//   const time = new Date().toLocaleTimeString();
+//   timeElement!.textContent = `The time is: ${time}`;
+
+//   chrome.storage.local.get(["timer"], (res) => {
+//     timer.innerText = `The time is: ${res.timer ?? 0} seconds`;
+//   });
+// };
+
+// updateTimeElements();
+// setInterval(() => {
+//   updateTimeElements();
+// }, 1000);
+
+// chrome.storage.sync.get(["name"], (res) => {
+//   userName.textContent = `Your name is: ${res.name ?? "???"}`;
+// });
+
+// chrome.action.setBadgeText(
+//   {
+//     text: "Time",
+//   },
+//   () => {
+//     console.log("Set the badge text");
+//   }
+// );
